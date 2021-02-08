deee573ae0cdb0bac4f478ab74d899f89ba7b17c
William Garrier <william.garrier63@orange.fr>
2021-02-08 14:53:45 +0100

    Project initialisation
    Need to change the paths to glpk in C/C++ and Data Link for Debug Win32 config. Also need to copy glpk_4_60.dll to Debug in project root.

---
+++ TP_RO/TP_RO/Betail.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <sstream>
+#include <stdlib.h>
+#include "glpk.h"
+
+using namespace std;
+
+class Betail
+{
+public:
+	void static solve();
+};
+
---
+++ TP_RO/TP_RO/TP_RO.cpp
@@ -0,0 +1,10 @@
+// TP_RO.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+
+#include <iostream>
+#include "Betail.h"
+
+int main()
+{
+    // Exercice 1 : Aliments pour bétail
+    Betail::solve();
+}
